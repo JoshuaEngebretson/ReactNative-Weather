7d982a51880587ce67b916814be58f489612579f
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-08-08 19:21:23 -0500

    Created components folder and created first new component
    - First created component is ForecastList.jsx

--- App.js
+++ App.js
@@ -1,44 +1,37 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	StatusBar,
+	SafeAreaView,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ForecastList from "./components/ForecastList";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-	const [timesPressed, setTimesPressed] = useState(0);
-
-	let textLog = "";
-	if (timesPressed > 1) {
-		textLog = timesPressed + "x onPress";
-	} else if (timesPressed > 0) {
-		textLog = "onPress";
-	}
-
 	return (
-		<View style={styles.container}>
-			<Text>Hello World!</Text>
-			<Pressable
-				onPress={() => {
-					setTimesPressed((current) => current + 1);
-				}}
-				style={({ pressed }) => [
-					{
-						backgroundColor: pressed
-							? "rgb(210, 230, 255)"
-							: "white",
-					},
-					styles.wrapperCustom,
-				]}
-			>
-				{({ pressed }) => (
-					<Text>{pressed ? "Pressed!" : "Press Me"}</Text>
-				)}
-			</Pressable>
-			<View style={styles.logBox}>
-				<Text testId="pressable_press_console">
-					{textLog}
-				</Text>
-			</View>
-			<StatusBar style="auto" />
-		</View>
+		<SafeAreaView style={styles.container}>
+			<StatusBar
+				backgroundColor="teal"
+				animated
+				barStyle={"default"}
+				hidden={false}
+			/>
+			<ForecastList />
+			<NavigationContainer>
+				{/* Similar to a Router */}
+				<Stack.Navigator initialRouteName="Weekly Forecast">
+					{/* Similar to Route */}
+					<Stack.Screen
+						name="Weekly Forecast"
+						component={ForecastList}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</SafeAreaView>
 	);
 }
 
@@ -48,19 +41,5 @@ const styles = StyleSheet.create({
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
-	},
-	wrapperCustom: {
-		borderRadius: 8,
-		padding: 6,
-	},
-	text: {
-		fontSize: 16,
-	},
-	logBox: {
-		padding: 20,
-		margin: 10,
-		borderWidth: StyleSheet.hairlineWidth,
-		borderColor: "#f0f0f0",
-		backgroundColor: "#f9f9f9",
 	},
 });
---
+++ components/ForecastList.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ForecastList() {
+	return (
+		<View>
+			<Text>List goes here...</Text>
+		</View>
+	);
+}
